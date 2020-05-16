1f6e070347538a90ab3d456b0c80931d2208d91c
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2020-05-15 23:54:32 -0300

    Obtendo Post por Id

---
+++ src/main/java/com/freitasfelipe/workshopmongo/resource/PostResource.java
@@ -0,0 +1,25 @@
+package com.freitasfelipe.workshopmongo.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.freitasfelipe.workshopmongo.domain.Post;
+import com.freitasfelipe.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
