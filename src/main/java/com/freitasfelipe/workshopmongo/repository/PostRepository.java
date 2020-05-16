94ffba848baf6665472922a083e72fe10b596a21
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2020-05-16 00:59:27 -0300

    Consulta com vários critérios

--- src/main/java/com/freitasfelipe/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/freitasfelipe/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.freitasfelipe.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -27,8 +28,11 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	 *	https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
 	 *	https://docs.mongodb.com/manual/reference/operator/query/regex/
 	 * 	
-	 * nome do campo, 0? == primeiro parametro, i == insensitivity
+	 * nome do campo, ?0 == primeiro parametro, i == insensitivity
 	 */
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
+	
+	@Query("{ $and: [ {date: {$gte: ?1} }, { date: { $lte: ?2} }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
