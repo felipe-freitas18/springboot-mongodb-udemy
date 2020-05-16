6eba7e964d4c9699eea264e309fbcbd9701e28e5
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2020-05-16 00:35:58 -0300

    Consulta simples com @Query

--- src/main/java/com/freitasfelipe/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/freitasfelipe/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.freitasfelipe.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.freitasfelipe.workshopmongo.domain.Post;
@@ -10,5 +11,24 @@ import com.freitasfelipe.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	/*
+	 * Usando query methods - SpringData gera a consulta pra gente
+	 * 
+	 * Referências:
+	 * https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
+	 * https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+	 * 
+	 * "Buscar posts contendo um dado string no título"
+	 */
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	/*
+	 *	https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
+	 *	https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+	 *	https://docs.mongodb.com/manual/reference/operator/query/regex/
+	 * 	
+	 * nome do campo, 0? == primeiro parametro, i == insensitivity
+	 */
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
