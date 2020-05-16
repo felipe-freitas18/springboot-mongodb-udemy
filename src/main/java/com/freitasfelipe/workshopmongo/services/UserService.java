48687c155f76611f596d5ccda4de2ad71cfa4948
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2020-05-15 22:37:14 -0300

    inserção de user com post

--- src/main/java/com/freitasfelipe/workshopmongo/services/UserService.java
+++ src/main/java/com/freitasfelipe/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.freitasfelipe.workshopmongo.domain.User;
+import com.freitasfelipe.workshopmongo.dto.UserDTO;
 import com.freitasfelipe.workshopmongo.repository.UserRepository;
 import com.freitasfelipe.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		 Optional<User> obj = repository.findById(id);
 		 return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
 	}
 }
